cc894bd988151a804cab47a51a72fa5778a0b555
Sergey Kulukov <onegreyonewhite@mail.ru>
2020-05-21 17:16:13 +1000

    Fix build on systems without glibc.

--- configparserc/include/_config.h
+++ configparserc/include/_config.h
@@ -1,11 +1,13 @@
 #include <string.h>
 #include <ctype.h>
 
+#ifdef __GLIBC__
 __asm__(".symver strlen,strlen@GLIBC_2.2.5");
 __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
 __asm__(".symver free,free@GLIBC_2.2.5");
 __asm__(".symver isspace,isspace@GLIBC_2.2.5");
 __asm__(".symver fmemopen,fmemopen@GLIBC_2.2.5");
+#endif
 
 int __has_only_whitespaces(char* s) {
     unsigned int i;
