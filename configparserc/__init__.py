3a6e8eb802e981b2e7caf27ffaf17ab212ddd1ed
Sergey Kulukov <onegreyonewhite@mail.ru>
2021-04-16 11:01:18 +1000

    Feature: build on aarch64 (experimental)
    WIP: onegreyonewhite/configparserc#1

--- configparserc/__init__.py
+++ configparserc/__init__.py
@@ -16,4 +16,4 @@ limitations under the License.
 
 """
 
-__version__ = '1.2.3'
+__version__ = '1.2.4'
--- configparserc/include/_config.h
+++ configparserc/include/_config.h
@@ -1,12 +1,15 @@
 #include <string.h>
 #include <ctype.h>
 
+#ifdef BUILD_FROM_SOURCE
+#else
 #ifdef __GLIBC__
 __asm__(".symver strlen,strlen@GLIBC_2.2.5");
 __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
 __asm__(".symver free,free@GLIBC_2.2.5");
 __asm__(".symver isspace,isspace@GLIBC_2.2.5");
 __asm__(".symver fmemopen,fmemopen@GLIBC_2.2.5");
+#endif
 #endif
 
 int __has_only_whitespaces(char* s) {
